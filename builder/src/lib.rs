4d01101f0b7f9552f1d2332ac9519cd6da3d856f
Matsuoka <quantumular@gmail.com>
2021-03-18 23:29:23 +0900

    pass test02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,33 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-    TokenStream::new()
+    let ast = parse_macro_input!(input as DeriveInput);
+    let struct_name = ast.ident;
+    let builder_name = format!("{}Builder", struct_name);
+    let builder_ident = syn::Ident::new(&builder_name, struct_name.span());
+
+    let expanded = quote! {
+        struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #struct_name {
+            fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
 }
